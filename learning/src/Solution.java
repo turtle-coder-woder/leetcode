7428eacc8cf90dc2e8a5a0e2d68bbfbfef9aa9c8
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-05-25 03:05:23 +0530

    'StockSpan#122'

---
+++ learning/src/Solution.java
@@ -0,0 +1,6 @@
+
+public class Solution {
+    public static void main(String args[]){
+        System.out.println(new StockSpan().maxProfit(new int[]{7,1,5,3,6,4}));
+    }
+}
---
+++ learning/src/StockSpan.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+public class StockSpan {
+    public int maxProfit(int[] prices) {
+        //after base condition about input , atleast 1 value will be present so we can safely init our stack
+        int maxProfit = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.add(prices[0]);
+        for (int day = 1; day < prices.length; day++) {
+            Integer localMax = 0;
+            Integer todayPrice = prices[day];
+            while (!stack.isEmpty() && stack.peek() <= todayPrice) {
+                localMax = Math.max(localMax, todayPrice - stack.pop());
+            }
+            stack.add(todayPrice);
+            maxProfit += localMax;
+
+        }
+        return maxProfit;
+    }
+}
