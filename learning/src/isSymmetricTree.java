bee60a283bcab44cdd1eb8e0da98a1489122350f
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-06-24 14:30:53 +0530

    isSymmetricTree

---
+++ learning/src/isSymmetricTree.java
@@ -0,0 +1,23 @@
+public class isSymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isSymmetricHelper(root.left, root.right);
+    }
+
+    Boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) { //if code some here means 1 node was null and other was not
+            return false;
+        }
+
+        if (left.val == right.val) {
+            return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+        }
+        return false;
+    }
+}
