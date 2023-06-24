27d26c13d866dc20e1f7d7856bbe130e8f50c7c2
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-06-24 14:07:14 +0530

    max depth in tree

---
+++ learning/src/MaxDepthInTree.java
@@ -0,0 +1,8 @@
+public class MaxDepthInTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
