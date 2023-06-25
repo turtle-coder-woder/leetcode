877c17104abdeee61b2a72eef98c48dfc7a77134
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-06-25 17:11:06 +0530

    delete a node from linkedList

---
+++ learning/src/DeleteNodeInALinkedList.java
@@ -0,0 +1,9 @@
+import helper.ListNode;
+
+public class DeleteNodeInALinkedList {
+    public void deleteNode(ListNode node) {
+        ListNode nextNode  = node.next;
+        node.next = nextNode.next;
+        node.val = nextNode.val;
+    }
+}
