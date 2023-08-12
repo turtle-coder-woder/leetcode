90d850baabde69772b5f1eb6735d5866d54e0c46
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-08-12 08:40:24 +0530

    MaxProfitForSingleTransaction using Kadane algo'

--- learning/src/MaximumSubarray.java
+++ learning/src/MaximumSubarray.java
@@ -1,16 +1,18 @@
 public class MaximumSubarray {
     public int maxSubArray(int[] nums) {
-        int globalmax = -100000;
-        int max = -100000;
+        int globalmax = Integer.MIN_VALUE;
+        int max = 0;
         for (int i = 0; i < nums.length; i++) {
             int current = nums[i];
-            if (current > max) {
-                max = Math.max(current, max + current);
-            } else {
-                max += current;
+            max += current;
+
+            if (max > globalmax) {
+                globalmax = max;
             }
-            globalmax = Math.max(max, globalmax);
+            if (max < 0) {
+                max = 0;
+            }
         }
-        return Math.max(max, globalmax);
+        return globalmax;
     }
 }
