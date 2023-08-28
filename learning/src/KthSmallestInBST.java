2ecf174cb1491bc54022e8375839a52bb95a8a84
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-08-28 19:20:11 +0530

    kth smallest in BST

---
+++ learning/src/KthSmallestInBST.java
@@ -0,0 +1,25 @@
+import easyCollection.TreeNode;
+
+import java.util.*;
+
+public class KthSmallestInBST {
+    public int kthSmallest(TreeNode root, int k) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k,Comparator.reverseOrder());
+        traverseWithHeap(root, maxHeap, k);
+        return maxHeap.peek();
+    }
+
+    void traverseWithHeap(TreeNode root, PriorityQueue<Integer> maxHeap, int size) {
+        if (root == null) {
+            return;
+        }
+        if (maxHeap.size() != size) {
+            maxHeap.add(root.val);
+        } else if (maxHeap.peek() > root.val) {
+            maxHeap.remove();
+            maxHeap.add(root.val);
+        }
+        traverseWithHeap(root.left, maxHeap, size);
+        traverseWithHeap(root.right, maxHeap, size);
+    }
+}
