74ca264156a35f5bc401928e500467291a2f0b36
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-09-27 10:44:54 +0530

    findpeak

---
+++ learning/src/FindPeak.java
@@ -0,0 +1,14 @@
+public class FindPeak {
+    public int findPeakElement(int[] nums) {
+        Integer peakIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[peakIndex]) {
+                return peakIndex;
+            }
+            peakIndex = i;
+        }
+
+        return peakIndex;
+
+    }
+}
