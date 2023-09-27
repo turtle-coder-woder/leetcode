12890b040b9b100d51a7041091035d6dcc32a15e
turtle-coder-woder <turtlecoderwoder@gmail.com>
2023-09-27 10:31:40 +0530

    find k largest

---
+++ learning/src/FindKLargest.java
@@ -0,0 +1,22 @@
+import java.util.Collection;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class FindKLargest {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue();
+        int index = 0;
+        for (; index < k; index++) {
+            minHeap.add(nums[index]);
+        }
+
+        for (; index < nums.length; index++) {
+            int currentElement = nums[index];
+            if (minHeap.peek() < currentElement) {
+                minHeap.remove();
+                minHeap.add(currentElement);
+            }
+        }
+        return minHeap.peek();
+    }
+}
