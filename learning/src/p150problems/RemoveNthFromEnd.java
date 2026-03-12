bab4d9437bd653d708874fa8b012d8f84a94b831
turtle-coder-woder <turtlecoderwoder@gmail.com>
2026-03-13 00:10:28 +0530

    remove Nth node from end iterative slow and fast pointer approach

--- learning/src/p150problems/RemoveNthFromEnd.java
+++ learning/src/p150problems/RemoveNthFromEnd.java
@@ -17,6 +17,34 @@ public class RemoveNthFromEnd {
         Other clever approach, slow and fast pointer
         move fast pointer till n
         and then move slow and fast pointer by 1 unit only to delete
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+        int i=0;
+        ListNode previousPointer = null;
+        ListNode slowPointer = head;
+        ListNode fastPointer = head;
+        while(i!=n && fastPointer!=null){
+            fastPointer=fastPointer.next;
+            i++;
+        }
+        while(fastPointer!=null){
+            previousPointer = slowPointer;
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next;
+        }
+
+        //now actual deletion
+        if(previousPointer==null){
+            //this means we need to delete from head first element
+            ListNode temp = head.next;
+            head.next=null;
+            head=temp;
+        }else{
+            previousPointer.next = slowPointer.next;
+            slowPointer.next=null;
+        }
+
+        return head;
+    }
         */
 
 
