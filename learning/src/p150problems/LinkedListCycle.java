03af40a80b4d213a60ec226367af10739f4c0cea
turtle-coder-woder <turtlecoderwoder@gmail.com>
2026-03-12 23:23:56 +0530

    iterativ version to detect cycle

--- learning/src/p150problems/LinkedListCycle.java
+++ learning/src/p150problems/LinkedListCycle.java
@@ -12,14 +12,17 @@ public class LinkedListCycle {
     If fast hits null → no cycle.
     */
     public boolean hasCycle(ListNode head) {
-        if(head==null || head.next==null){
-            return false;
-        }
-
         ListNode slowPointer = head;
-        ListNode fastPointer = head.next.next;
+        ListNode fastPointer = head;
 
-        return hasCycleSlowAndFastPointers(slowPointer,fastPointer);
+        while(fastPointer!=null && fastPointer.next!=null){
+            slowPointer=slowPointer.next;
+            fastPointer=fastPointer.next.next;
+            if(slowPointer==fastPointer){
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean hasCycleSlowAndFastPointers(ListNode slowPointer, ListNode fastPointer) {
